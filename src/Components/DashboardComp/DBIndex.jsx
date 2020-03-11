7cce87442b7b3a7b54392d48763163c04a65efb9
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2020-03-11 13:04:54 +0000

    added the boiler plate for the summary & report charts

--- src/Components/DashboardComp/DBIndex.jsx
+++ src/Components/DashboardComp/DBIndex.jsx
@@ -4,9 +4,45 @@ import DBExpenses from './DBExpenses'
 import DBRevenue from './DBRevenue'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import DBSummary from './DBSummary'
-
+import {connect} from 'react-redux'
 export class DBIndex extends Component {
+    
     render() {
+        var revArr = this.props.revenueTransaction
+        var expArr = this.props.expenseTransaction
+        // console.log(expArr)
+
+
+        var cashPmt = 0;
+        var mobileMoney = 0;
+        var bankTransfer = 0;
+        for (var i =0; i<revArr.length; i++) {
+            if (revArr[i].pmtMode=== 'Cash Payment') {
+                cashPmt += parseFloat(revArr[i].saleAmount)
+            }else if (revArr[i].pmtMode ==='Mobile Money') {
+                mobileMoney += parseFloat(revArr[i].saleAmount)
+            }else{
+                bankTransfer += parseFloat(revArr[i].saleAmount)
+            }
+        }
+        var expCashPmt = 0;
+        var expMobileMoney = 0;
+        var expBankTransfer = 0;
+        for (var e =0; e<expArr.length; e++) {
+            if (expArr[e].paymentMode === "Cash-In-Hand") {
+                expCashPmt += parseFloat(expArr[e].expAmount)
+            }else if (expArr[e].paymentMode ==='Mobile Money') {
+                expMobileMoney += parseFloat(expArr[e].expAmount)
+            }else{
+                expBankTransfer += parseFloat(expArr[e].expAmount)
+            }
+        }
+
+        const netCashBalance = cashPmt - expCashPmt;
+        const netMobileMoneyBalance = mobileMoney - expMobileMoney;
+        const netBankBalance = bankTransfer - expBankTransfer
+        const totalBalance = netBankBalance + netMobileMoneyBalance + netCashBalance
+        
         return (
             <BrowserRouter>
                 <div className='dbIndex'>
@@ -15,7 +51,13 @@ export class DBIndex extends Component {
                     </div>
                     <div className="col-md-9">
                         <Switch>
-                            <Route path='/' exact  component={DBSummary}/>
+                            <Route path='/' exact  render={
+                                (props) => <DBSummary {...props} 
+                                netCashBalance={netBankBalance}
+                                netMobileMoneyBalance={netMobileMoneyBalance} 
+                                netBankBalance={netBankBalance} 
+                                totalBalance={totalBalance}/>
+                            }/>
                             <Route path='/dbexpense'  component={DBExpenses}/>
                             <Route path='/dbrevenue'  component={DBRevenue}/>
                         </Switch>
@@ -25,5 +67,11 @@ export class DBIndex extends Component {
         )
     }
 }
-
-export default DBIndex
+const mapStateToProps = (state) => {
+    const { expenseTransaction, revenueTransaction } = state
+    return {
+        expenseTransaction,
+        revenueTransaction
+    }
+}
+export default connect (mapStateToProps, null) (DBIndex)
--- src/Components/DashboardComp/DBSummary.jsx
+++ src/Components/DashboardComp/DBSummary.jsx
@@ -1,48 +1,26 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+
+
+const data = [
+    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400,},
+    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210,},
+    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290,},
+    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000,},
+    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181,},
+    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500,},
+    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100,},
+];
 
 export class DBSummary extends Component {
-  
 
 
 
-    render() {
-
-        var revArr = this.props.revenueTransaction
-        var expArr = this.props.expenseTransaction
-        // console.log(expArr)
 
 
-        var cashPmt = 0;
-        var mobileMoney = 0;
-        var bankTransfer = 0;
-        for (var i =0; i<revArr.length; i++) {
-            if (revArr[i].pmtMode=== 'Cash Payment') {
-                cashPmt += parseFloat(revArr[i].saleAmount)
-            }else if (revArr[i].pmtMode ==='Mobile Money') {
-                mobileMoney += parseFloat(revArr[i].saleAmount)
-            }else{
-                bankTransfer += parseFloat(revArr[i].saleAmount)
-            }
-        }
-        var expCashPmt = 0;
-        var expMobileMoney = 0;
-        var expBankTransfer = 0;
-        for (var e =0; e<expArr.length; e++) {
-            if (expArr[e].paymentMode === "Cash-In-Hand") {
-                expCashPmt += parseFloat(expArr[e].expAmount)
-            }else if (expArr[e].paymentMode ==='Mobile Money') {
-                expMobileMoney += parseFloat(expArr[e].expAmount)
-            }else{
-                expBankTransfer += parseFloat(expArr[e].expAmount)
-            }
-        }
+    render(props) {
+        const { netCashBalance, netBankBalance, netMobileMoneyBalance, totalBalance } = this.props
 
-        const netCashBalance = cashPmt - expCashPmt;
-        const netMobileMoneyBalance = mobileMoney - expMobileMoney;
-        const netBankBalance = bankTransfer - expBankTransfer
-        const totalBalance = netBankBalance + netMobileMoneyBalance + netCashBalance
-        
 
         return (
             <div className='right'>
@@ -75,15 +53,77 @@ export class DBSummary extends Component {
                 </div>
                 <div className="col-md-7">
                     <ul className="nav nav-tabs" role="tablist">
-                        <li role="presentation" className="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab"><b>Expenses</b></a></li>
-                        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab"><b>Revenue</b></a></li>
-                        <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab"><b>Performance</b></a></li>
+                        <li role="presentation" className="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab"><b>Net Cash-In-Hand</b></a></li>
+                        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab"><b>Net Bank Balance</b></a></li>
+                        <li role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab"><b>Mobile Money</b></a></li>
+                        <li role="presentation"><a href="#cashPosition" aria-controls="messages" role="tab" data-toggle="tab"><b>Cash Position</b></a></li>
                     </ul>
 
                     <div className="tab-content">
-                        <div role="tabpanel" className="tab-pane active" id="home">Expense chart</div>
-                        <div role="tabpanel" className="tab-pane" id="profile">Revenue Chart</div>
-                        <div role="tabpanel" className="tab-pane" id="messages">Performance Chart</div>
+                        <div role="tabpanel" className="tab-pane active" id="home">
+                            <AreaChart
+                                width={700}
+                                height={400}
+                                data={data}
+                                margin={{
+                                    top: 10, right: 30, left: 0, bottom: 0,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                            </AreaChart>
+                        </div>
+                        <div role="tabpanel" className="tab-pane" id="profile">
+                        <AreaChart
+                                width={700}
+                                height={400}
+                                data={data}
+                                margin={{
+                                    top: 10, right: 30, left: 0, bottom: 0,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="uv" stroke="#D4D884" fill="#D4D884" />
+                            </AreaChart>
+                        </div>
+                        <div role="tabpanel" className="tab-pane" id="messages">
+                        <AreaChart
+                                width={700}
+                                height={400}
+                                data={data}
+                                margin={{
+                                    top: 10, right: 30, left: 0, bottom: 0,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="uv" stroke="#D87575" fill="#D87575" />
+                            </AreaChart>
+                        </div>
+                        <div role="tabpanel" className="tab-pane" id="cashPosition">
+                        <AreaChart
+                                width={700}
+                                height={400}
+                                data={data}
+                                margin={{
+                                    top: 10, right: 30, left: 0, bottom: 0,
+                                }}
+                            >
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="uv" stroke="#D87575" fill="#D87575" />
+                            </AreaChart>
+                        </div>
                     </div>
                     <div className="col-md-1"></div>
                 </div>
@@ -91,13 +131,6 @@ export class DBSummary extends Component {
         )
     }
 }
-const mapStateToProps = (state) => {
-    const { expenseTransaction, revenueTransaction } = state
-    return {
-        expenseTransaction,
-        revenueTransaction
-    }
-}
 
-export default connect(mapStateToProps, null)(DBSummary)
+export default DBSummary
 
