9d9a44729d936f6a49d22142fbb7bc7b5336aa97
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2020-03-12 16:58:43 +0000

    chart report completed

--- src/Components/ReportComp/Chart.jsx
+++ src/Components/ReportComp/Chart.jsx
@@ -5,26 +5,24 @@ import {
 
 const data = [
     {
-        name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+        name: '2020-01-2', uv: 19365, pv: 2400, amt: 2400,
     },
     {
-        name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+        name: '2020-01-9', uv: 14524, pv: 1398, amt: 2210,
     },
     {
-        name: 'Page C', uv: -1000, pv: 9800, amt: 2290,
+        name: '2020-01-23', uv: -9760, pv: 9800, amt: 2290,
     },
     {
-        name: 'Page D', uv: 500, pv: 3908, amt: 2000,
+        name: '2020-01-30', uv: 987, pv: 3908, amt: 2000,
     },
     {
-        name: 'Page E', uv: -2000, pv: 4800, amt: 2181,
+        name: '2020-02-06', uv: 3682, pv: 4800, amt: 2181,
     },
     {
-        name: 'Page F', uv: -250, pv: 3800, amt: 2500,
+        name: '2020-02-11', uv: 24206, pv: 3800, amt: 2500,
     },
-    {
-        name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-    },
+
 ];
 
 const gradientOffset = () => {
